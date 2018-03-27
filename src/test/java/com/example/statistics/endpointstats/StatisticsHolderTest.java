f1ba5753fc6220af120feab83bcc5e983dd7a2d9
Paolo Venturini <paol3tto@gmail.com>
2018-03-27 21:51:42 +0200

    ref: unused import

--- src/test/java/com/example/statistics/endpointstats/StatisticsHolderTest.java
+++ src/test/java/com/example/statistics/endpointstats/StatisticsHolderTest.java
@@ -6,7 +6,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import static org.junit.Assert.assertEquals;
